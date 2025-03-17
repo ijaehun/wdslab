e613fc0cbc08d2555472ba758574b58983594ca6
HoonyHoony <dynamic97312@naver.com>
2025-03-17 16:48:08 +0900

    update docker compose

--- wdslab_backend/wdslab_backend/settings.py
+++ wdslab_backend/wdslab_backend/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 """
 
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -94,11 +95,10 @@ DATABASES = {
         'NAME': 'wds',
         'USER': 'postgres',
         'PASSWORD': '0312',
-        'HOST': 'localhost',
+        'HOST': os.environ.get('DB_HOST', 'localhost'),  # 환경 변수로 제어
         'PORT': '5432',
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/4.2/ref/settings/#auth-password-validators
